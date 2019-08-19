67eea6c427c92ba19c38e59d25cc4a98c20fc1fc
Alex Lévy <alexlevy0@gmail.com>
2019-08-19 11:30:19 +0200

    Prevent loosing focus when on web

--- src/Row.js
+++ src/Row.js
@@ -1,6 +1,6 @@
 import React, {Component, cloneElement} from 'react';
 import PropTypes from 'prop-types';
-import {Animated, PanResponder, StyleSheet, ViewPropTypes} from 'react-native';
+import {Animated, PanResponder, StyleSheet, ViewPropTypes, Platform} from 'react-native';
 import {shallowEqual} from './utils';
 
 export default class Row extends Component {
@@ -82,15 +82,21 @@ export default class Row extends Component {
     },
 
     onPanResponderMove: (e, gestureState) => {
-      if (
+      if (Platform.OS !== 'ios' && Platform.OS !== 'android') {
+        if (!this._active || gestureState.numberActiveTouches > 1) {
+          if (!this._isTouchInsideElement(e)) {
+            this._cancelLongPress();
+          }
+          return;
+        }
+      } else if (
         !this._active ||
-        gestureState.numberActiveTouches > 1 ||
-        e.nativeEvent.target !== this._target
+        gestureState.numberActiveTouches > 1 
+        || e.nativeEvent.target !== this._target
       ) {
         if (!this._isTouchInsideElement(e)) {
           this._cancelLongPress();
         }
-
         return;
       }
 
